f1de2f4a7133b29edd4d9a920c50fd1d5d9fb132
Aksel Skaar Leirvaag <akselsle@mobai.bio>
2021-06-15 16:52:50 +0200

    added test for grep

--- main_test.go
+++ main_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	is2 "github.com/matryer/is"
+	IS "github.com/matryer/is"
 	"strings"
 	"testing"
 )
 
 func TestParseGrepOutput(t *testing.T) {
-	is := is2.New(t)
+	is := IS.New(t)
 	expectedNames := []string{"config.json", "go.mod", "results.json", "test.txt"}
 	testInput := []string{
 		"repository-words-grepper/config.json:fell",
@@ -19,9 +19,32 @@ func TestParseGrepOutput(t *testing.T) {
 	}
 
 	parsed := parseGrepOutput(strings.Join(testInput, "\n"), "repository-words-grepper")
-	
+
 	is.Equal(len(expectedNames), len(parsed))
 	for i, result := range parsed {
 		is.Equal(expectedNames[i], result.FileName)
 	}
+}
+
+func TestGrep(t *testing.T) {
+	is := IS.New(t)
+	expectedResult := []GrepResult{
+		{
+			"testdata_1.txt",
+			2,
+		},
+		{
+			"testdata_2.txt",
+			4,
+		},
+	}
+	result, err := grep("./testdata", []string{"fell"}, []string{})
+
+	is.NoErr(err)
+	is.Equal(len(expectedResult), len(result))
+	for i, r := range result {
+		is.Equal(expectedResult[i].FileName, r.FileName)
+		is.Equal(expectedResult[i].Count, r.Count)
+	}
+
 }
